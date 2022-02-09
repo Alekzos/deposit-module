2d505309b4e87585db56a7e73a8be17a05bc53d1
Alekzos <62596640+Alekzos@users.noreply.github.com>
2022-02-09 19:11:53 +0300

    доработаны срок, сумма депозита добавлена периодичность выплат
    Исправлена ошибка и доработан срока вклада
    
    бегунок и текстовое поле выделены в отдельный компонент
    
    Правки в стилях, удалил лишние импорты
    Добавлена периодичность выплат

--- src/components/DepositCalc.tsx
+++ src/components/DepositCalc.tsx
@@ -1,106 +1,88 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+
+import { useState } from 'react';
 
 import './style.css';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import Divider from '@mui/material/Divider';
-import Slider from '@mui/material/Slider';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { StylesProvider } from '@mui/styles';
-import { withStyles } from '@mui/styles';
-import { createStyles, makeStyles } from '@mui/styles';
-import { styled } from '@mui/material/styles';
-import customStyles from './styles'
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import MuiInput from '@mui/material/Input';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
+import Box from '@mui/material/Box'
+
+import {numberWithoutSpaces} from './utils';
+
+import SliderWithTextField from './SliderWithTextField'
+
 
 
 
 //main
 const DepositCalc = () => {
-  type Nullable<T> = T | undefined | null;
+
+  enum Currency { 
+    rub = "rub", 
+    usd = "usd",
+  };
+
+  enum paymentPeriods { 
+    startOfTerm = "startOfTerm", 
+    monthly = "monthly",
+    endOfTerm = "endOfTerm",
+  };
+  
 
   //переключатель валюты
-  const [currency, setCurrency] = useState<string | null | undefined>('ruble');
+  const [currency, setCurrency] = useState<string>(Currency.rub);
+
   const handleCurrency = (
     event: React.MouseEvent<HTMLElement>,
-    newCurrency: string | null | undefined,
+    newCurrency: string,
   ) => {
     setCurrency(newCurrency);
-    console.log(newCurrency)
   };
+
+  //выбор периода платежей
+  const [paymentPeriod, setPaymentPeriod] = useState<string>(paymentPeriods.startOfTerm);;
+
+  const handlePaymentPeriod = (
+    event: React.MouseEvent<HTMLElement>,
+    newPeriod: string,
+  ) => {
+    setPaymentPeriod(newPeriod);
+  };
+
 
 
   // выбор суммы вклада 
-  const [value, setValue] = useState<number | string | Array<number | string>>(
-    0,
-  );
+  const [value, setValue] = useState<number>(0,);
 
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue);
+    setValue(+newValue);
   };
-
-  function numberWithSpaces(val: any) {
-    val = Number(val.toString().replace(' ', ''));
-    let formatedNumber = val.toLocaleString();
-    return formatedNumber;
-  }
-
-  function numberWithoutSpaces(val: any) {
-      val = Number(val.toLocaleString().replace(/\D/g,''));
-      if (val >= 100000000) {val = 100000000} // пре превышении лимита установить макс значение
-    return val;
-  }
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value === '' ? '' : numberWithoutSpaces(event.target.value));
+    setValue(event.target.value === '' ? '' : numberWithoutSpaces(event.target.value, 100000000));
   };
-
-  const marksSliderDeposit = [
-    {
-      value: 0,
-      label: `0 ${(currency === 'ruble'? '₽' : '$')}`,
-    },
-    {
-      value: 100000000,
-      label: `∞ ${(currency === 'ruble'? '₽' : '$')}`,
-    },
-  ];
 
 
   //выбор срока депозита
-  const [depositTerm, setdepositTerm] = useState<number | string | Array<number | string | null | undefined>>(
-    1,
-  );
+  const [depositTerm, setdepositTerm] = useState<number>(1,);
+
+
+  const handleInputDepositTerm = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value === '' ? '' : numberWithoutSpaces(event.target.value, 1095));
+  };
 
   const handleSliderDepositTerm = (event: Event, newValue: number | number[]) => {
-    setValue(newValue);
+    setdepositTerm(+newValue);
   };
-  
-  const marksSliderDepositTerm = [
-    {
-      value: 1,
-      label: '1 день',
-    },
-    {
-      value: 1095,
-      label: '3 года',
-    },
-  ];
 
 
-  
-  
+    
+  const days: boolean = true;
+
     return (
     <>
     <StyledEngineProvider injectFirst>
@@ -108,96 +90,78 @@ const DepositCalc = () => {
       <Typography paragraph>Введите параметры и подберите подходящий продукт</Typography>
       <Grid container>
         <Grid item xs={6}> 
+          <Box>
+            <ToggleButtonGroup
+              value={currency}
+              exclusive
+              onChange={handleCurrency}
+              aria-label="currency"
+              color="primary"
+              fullWidth
+            >
+              <ToggleButton  value="rub">Рубли</ToggleButton>     
+              <ToggleButton  value="usd">Доллары США</ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+
+          <SliderWithTextField 
+            caption={'Сумма'}
+            Currency={Currency}
+            currency={currency} 
+            step={500}
+            min= {0}
+            max = {100000000}
+            value= {value}
+            handleInputChange = {handleInputChange}
+            handleSliderChange = {handleSliderChange}
+          />
+
+ 
+          <SliderWithTextField 
+            caption={'Срок'}
+            days
+            Currency={Currency}
+            step={1}
+            min= {1}
+            max = {1095}
+            value= {depositTerm}
+            handleInputChange = {handleInputDepositTerm}
+            handleSliderChange = {handleSliderDepositTerm}
+          />
+
+          <Box>
+          <Typography variant='caption'>Выплата процентов</Typography>  
           <ToggleButtonGroup
-            value={currency}
+            value={paymentPeriod}
             exclusive
-            onChange={handleCurrency}
-            aria-label="currency"
+            onChange={handlePaymentPeriod }
+            aria-label="period-payment"
             color="primary"
             fullWidth
           >
-            <ToggleButton  value="ruble">Рубли</ToggleButton>
-            <Divider variant="middle" />        
-            <ToggleButton  value="dollar">Доллары США</ToggleButton>
+            <ToggleButton  value="startOfTerm">В начале срока</ToggleButton>     
+            <ToggleButton  value="monthly">Ежемесячно</ToggleButton>
+            <ToggleButton  value="endOfTerm">В конце срока</ToggleButton>
           </ToggleButtonGroup>
-
-          <Box>
-            <Typography variant='caption'>Сумма</Typography>   
-            <TextField
-              hiddenLabel
-              id="outlined-number"
-              defaultValue="0"
-              variant="outlined"
-              size="small"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              fullWidth
-              value={numberWithSpaces(value)}
-              //value={numberWithSpaces(value) + '<p className={"currency"}> ₽</p>'}
-              onChange={handleInputChange}
-              inputProps={{
-                step: 500,
-                min: 0,
-                'aria-labelledby': 'input-slider',
-              }}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">{(currency === 'ruble'? '₽' : '$')}</InputAdornment>, 
-              }}
-
-            /> 
-
-            <Slider
-              value={typeof value === 'number' ? value : 0}
-              onChange={handleSliderChange}
-              aria-labelledby="input-slider"
-              min={0}
-              max = {100000000}
-              step={500}
-              valueLabelFormat={numberWithSpaces(value) + (currency === 'ruble'? ' ₽' : ' $')}
-              valueLabelDisplay="auto"
-              marks={marksSliderDeposit}    
- 
-            />
-          </Box>
-
-          <Box>
-            <Typography variant='caption'>Срок</Typography>   
-            <Slider
-              value={typeof depositTerm === 'number' ? value : 1}
-              //value={1}
-              onChange={handleSliderDepositTerm}
-              aria-labelledby="DepositTermSlider"
-              min={1}
-              max = {1095}
-              step={1}
-              //valueLabelFormat={numberWithSpaces(value) + (currency === 'ruble'? ' ₽' : ' $')}
-              valueLabelDisplay="auto"
-              marks={marksSliderDepositTerm} 
-            />
           </Box>
         
+        
         <br></br>
-        выплата процентов<br></br>
+        
         опции<br></br>
         сохранить расчет
         </Grid>
            
         <Grid item xs={6}>
+          <Box>
             Варианты вкладов<br></br>
-
             авансовый<br></br>
             универсальный<br></br>
             фиксированный<br></br>
             гибкий
-
+            </Box>
         </Grid>
       </Grid>
-      
-       
-
-
-      
       </StyledEngineProvider>
     </>
     )};
---
+++ src/components/SliderWithTextField.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+import { useState } from 'react';
+import './style.css';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import { Tooltip } from '@mui/material';
+import HelpOutline from '@mui/icons-material/HelpOutline';
+import {numberWithSpaces} from './utils';
+
+
+interface SliderWithTextFieldProps {
+    Currency?: any,
+    currency?:string,
+    days?:boolean,
+    step: number,
+    min: number,
+    max: number,
+    value: number,
+    caption:string,
+    handleInputChange?: any,
+    handleSliderChange?: any,
+}
+
+const SliderWithTextField: React.FC<SliderWithTextFieldProps> = (
+    {caption, Currency, currency, days, step, min, max, value, handleInputChange, handleSliderChange}) => {
+               
+
+        //метки для бегунка с датами
+        const marksSliderDepositTerm = [
+            {
+                value: 1,
+                label: '1 день',
+            },
+            {
+                value: 180,
+                label: 'Полгода',
+            },
+            {
+                value: 365,
+                label: '1 год',
+            },
+            {
+                value: 548,
+                label: '1.5 года',
+            },
+            {
+                value: 730,
+                label: '2 года',
+            },
+            {
+                value: 913,
+                label: '2.5 года',
+            },
+            {
+                value: 1095,
+                label: '3 года',
+            },
+        ];
+
+        //метки для бегунка с валютой
+        const marksSliderDeposit = [
+            {
+                value: 0,
+                label: `0 ${(currency === Currency.rub? '₽' : '$')}`,
+            },
+            {
+                value: 100000000,
+                label: `∞ ${(currency === Currency.rub? '₽' : '$')}`,
+            },
+            ];
+        
+            
+  
+    return(
+        <Box>         
+            <Typography variant='caption'>{caption}</Typography>  
+
+            <TextField
+              hiddenLabel
+              id="outlined-number"
+              defaultValue="0"
+              variant="outlined"
+              size="small"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              fullWidth
+              value={numberWithSpaces(value)}
+              onChange={handleInputChange}
+              inputProps={{
+                step: {step},
+                min: {min},
+                'aria-labelledby': 'input-slider',
+              }}
+              InputProps={{
+                endAdornment: 
+                <InputAdornment position="end">
+                    {(days)? "дней"  : (currency === Currency.rub? '₽' : '$')} 
+
+                    {(days)? 
+                    <Tooltip title="Информация о сроках">
+                       <HelpOutline />
+                    </Tooltip> : ''}  
+                 </InputAdornment>,
+
+
+            }}
+
+            /> 
+
+            {/* бегунок */}
+            <Slider
+              value={value}
+              onChange={handleSliderChange}
+              aria-labelledby="input-slider"
+              min={min}
+              max = {max}
+              step= {step}
+              valueLabelFormat={
+              (days)? value + ' дней' : 
+              numberWithSpaces(value) + (currency === Currency.rub? ' ₽' : ' $')
+              } 
+              valueLabelDisplay="auto"
+              marks={
+                (days)? marksSliderDepositTerm : marksSliderDeposit
+                }   
+            />
+        </Box>
+    )
+}
+export default SliderWithTextField
